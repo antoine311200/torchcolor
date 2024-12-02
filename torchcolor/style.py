1e6f352b6cb2e19de282a5fdc9bc3d1e862406c9
antoine311200 <antoine311200@gmail.com>
2024-12-02 09:43:07 +0100

    Docstring on style.py

--- torchcolor/style.py
+++ torchcolor/style.py
@@ -12,6 +12,9 @@ def clean_style(text: str) -> str:
 
 @dataclass
 class TextStyle:
+    """Text style dataclass with foreground and background colors/gradients and additional properties
+    such as underline, italic and so on."""
+
     # May add support for underline style later [escape code 58]
     fg_style: Union[Gradient, Color, str, tuple[int, int, int]] = None
     bg_style: Union[Gradient, Color, str, tuple[int, int, int]] = None
@@ -24,16 +27,34 @@ class TextStyle:
     darken: bool = False
 
     def __post_init__(self):
+        """Ensure that the foreground and background style are converted into a Color or Gradient object"""
         self.fg_style = self._ensure_style(self.fg_style)
         self.bg_style = self._ensure_style(self.bg_style)
 
     @staticmethod
-    def _ensure_style(value: Union[Gradient, Color, str, tuple[int, int, int]]) -> Color:
+    def _ensure_style(value: Union[Gradient, Color, str, tuple[int, int, int]]) -> Union[Color, Gradient]:
+        """Convert any non Color or Gradient object into a Color assuming an hexadecimal code, RGB tuple or preset string color
+        has been specified.
+
+        Args:
+            value (Union[Gradient, Color, str, tuple[int, int, int]]): A color value either as a Gradient, Color, rgb tuple, preset or hex code
+
+        Returns:
+            Union[Color, Gradient]: Converted style
+        """
         if isinstance(value, Color) or isinstance(value, Gradient):
             return value
         return Color(value)
 
     def apply(self, text: str) -> str:
+        """Apply the style properties to the given text
+
+        Args:
+            text (str): A string
+
+        Returns:
+            str: Stylised text
+        """
         bold_ansi = "\033[1m" if self.bold else ""
         darken_ansi = "\033[2m" if self.darken else ""
         italic_ansi = "\033[3m" if self.italic else ""
@@ -94,48 +115,58 @@ def colorize(
     return TextStyle(foreground_color, background_color).apply(text)
 
 
-
+# Functional Types
 DelimiterType = Literal["delimiter"]
 KeyType = Literal["key"]
 AnyType = Literal["any"]
 FunctionalType = Union[Type, DelimiterType, KeyType, AnyType]
 
 def infer_type(value: str):
+    """Naive type inference that is sufficient for nn.Layer parsing"""
     if value.lower() == "true" or value.lower() == "false":
         return bool
-
     try:
         int(value)
         return int
-    except ValueError:
-        pass
-
+    except ValueError: pass
     try:
         float(value)
         return float
-    except ValueError:
-        pass
-
+    except ValueError: pass
     try:
-        if value[0] == "#":
-            return str
-    except:
-        pass
-
+        if value[0] == "#": return str
+    except: pass
     return KeyType
 
 LAYER_SPLITTER = r'\s*[=,]\s*'
 
 @dataclass
 class FunctionalStyle:
+    """Functional styling class that can split a string with regular expression
+    and infer each match to a specific style.
+    """
     styles: defaultdict[FunctionalType, TextStyle]
+    infer_func: function = infer_type
     splitter: str
 
     def __post_init__(self):
+        """Ensure that the styles dictionary is a default dict to avoid any inference issue"""
         if not isinstance(self.styles, defaultdict):
             self.styles = defaultdict(TextStyle, self.styles)
 
-    def apply(self, text):
+    def apply(self, text: str) -> str:
+        """Apply the functional style to the text.
+
+        The method uses the splitting regex pattern to separate the text into
+        multiple matched strings and their delimiters.
+        Then, infer the string to link the corresponding style.
+
+        Args:
+            text (str): A string
+
+        Returns:
+            str: Stylised string
+        """
         matches = re.split(f'({self.splitter})', text)
 
         result = []
@@ -149,7 +180,7 @@ class FunctionalStyle:
         for res, is_delim in result:
             if not is_delim: stylised_text += self.styles[DelimiterType].apply(res)
             else:
-                found_type = infer_type(res)
+                found_type = self.infer_func(res)
                 if found_type not in self.styles:
                     found_type = AnyType
                 stylised_text += self.styles[found_type].apply(res)
